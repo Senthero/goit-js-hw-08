dc302e3291681678b7a5ef30166c0a5c31878ba9
Senthero <marou.ra3000@gmail.com>
2024-05-10 15:14:59 +0300

    add_code

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach((item) => {
-  const categorName = item.querySelector("h2").textContent;
-  const categorItemNum = item.querySelectorAll("li");
-  console.log(`Category: ${categorName}`);
-  console.log(`Elements: ${categorItemNum.length}`);
-});
